cf57c9a1d310dc32a01cc53ff50f06dc8a59985e
unknown <frfontoura@gmail.com>
2018-01-30 19:47:06 -0200

    Criando carrinho de compras

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { STORAGE_KEYS } from "../config/storage_keys.config";
 import { LocalUser } from "../models/local_user";
+import { Cart } from "../models/cart";
 
 
 @Injectable()
@@ -19,6 +20,22 @@ export class StorageService {
             localStorage.removeItem(STORAGE_KEYS.localUser);
         } else {
             localStorage.setItem(STORAGE_KEYS.localUser, JSON.stringify(usr));
+        }
+    }
+
+    getCart() : Cart {
+        let cart = localStorage.getItem(STORAGE_KEYS.cart);
+        if(cart == null) {
+            return null;
+        }
+        return JSON.parse(cart);
+    }
+
+    setCart(cart: Cart) {
+        if(cart == null) {
+            localStorage.removeItem(STORAGE_KEYS.cart);
+        } else {
+            localStorage.setItem(STORAGE_KEYS.cart, JSON.stringify(cart));
         }
     }
 
